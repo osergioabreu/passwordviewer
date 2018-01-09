dd31f0328c171f4de4e0d7829f27ecc010bc1b6d
osergioabreu <34313003+osergioabreu@users.noreply.github.com>
2018-01-09 11:22:48 -0200

    Update passview.js
    Simplified making iconTarget optional.
    Added style property for the eye

--- passview.js
+++ passview.js
@@ -1,14 +1,19 @@
 /* 
  * Password viewer by Sergio Abreu
  * 06-12-2017
+ * Updated: 09-01-2018
  * http://sites.sitesbr.net
  * Requires glyphicons  
  */
 
 var SA_PassView = {
-   add: function(obj){ 
-          
+   add: function(obj){           
      var sp = document.createElement('span'), action='visualizar';
+     if(! obj.iconTarget){
+       obj.iconTarget = obj.domElem.parentNode;
+       obj.style = 'margin-left: 10px';
+     }
+     if( obj.style) sp.setAttribute("style", obj.style);
      sp.style.fontSize = "1.1em";
      sp.setAttribute('class', "glyphicon glyphicon-eye-close");
      sp.setAttribute("title", "Clique para "+action+" a senha");
@@ -28,10 +33,19 @@ var SA_PassView = {
      });
      obj.iconTarget.appendChild(sp);
    },
-   /* obj is:
-    * { domElem: (the form field), 
-    *   iconTarget: (dom element to place the icon)
-    *  }
+   /* Basic obj is:
+    * { 
+    *   domElem: (the form field)
+    * }
+    *  
+    * If you want to place the eye in a specific DOM element, use iconTarget property.
+    * 
+    *    iconTarget: (dom element to place the icon)
+    *  
+    * If ommited iconTarget, the domElem.parentNode will be used and margin-left is added automatically.
+    *  
+    * Accepts also a style property, holding the string for initial style for the eye: 
+    *   style: 'margin-left: 10px' 
     */
   
 };
